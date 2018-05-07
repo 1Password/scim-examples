21a20a00179210f61aeeaea7ca7a5234b28b51b6
agilecohix <connor@agilebits.com>
2018-05-07 16:52:21 -0400

    Changed to the correct image name

--- kubernetes/create-session-file.sh
+++ kubernetes/create-session-file.sh
@@ -2,7 +2,7 @@
 
 mkdir session
 
-docker run -it -v $PWD/session:'/op-scim/session' scimtest /op-scim/create-session-docker.sh
+docker run -it -v $PWD/session:'/op-scim/session' onepassword/scim /op-scim/create-session-docker.sh
 
 cp ./session/scimsession ./scimsession
 rm -rf ./session
