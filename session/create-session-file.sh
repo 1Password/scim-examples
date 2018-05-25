1c8e9ab809bae264449eb27e71b55cbc5a18f53a
agilecohix <connor@agilebits.com>
2018-05-25 14:37:30 -0400

    restored create-session-file.sh

---
+++ session/create-session-file.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+mkdir session
+
+docker run -it -v $PWD/session:'/op-scim/session' onepassword/scim /op-scim/create-session-docker.sh
+
+cp ./session/scimsession ./scimsession
+rm -rf ./session 
