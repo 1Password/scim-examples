0cfcf992ee56c1e9d8d3634cc1f6a1502f8a1309
agilecohix <connor@agilebits.com>
2018-05-25 14:37:56 -0400

    Removed unneed space

--- session/create-session-file.sh
+++ session/create-session-file.sh
@@ -5,4 +5,4 @@ mkdir session
 docker run -it -v $PWD/session:'/op-scim/session' onepassword/scim /op-scim/create-session-docker.sh
 
 cp ./session/scimsession ./scimsession
-rm -rf ./session 
+rm -rf ./session
