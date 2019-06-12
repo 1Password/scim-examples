ad3a27d0cb4b135e7ab70791ff0b676979a344a3
agilecohix <connor@agilebits.com>
2019-06-12 09:53:14 -0400

    updated for version 0.8.4

--- scim-setup.sh
+++ scim-setup.sh
@@ -2,7 +2,7 @@
 
 mkdir session
 
-docker run -it -v $PWD/session:'/op-scim/session' -v $HOME/.op:'/root/.op' 1password/scim:v0.8.3 /op-scim/create-session-docker.sh
+docker run -it -v $PWD/session:'/op-scim/session' -v $HOME/.op:'/root/.op' 1password/scim:v0.8.4 /op-scim/create-session-docker.sh
 
 cp ./session/scimsession ./scimsession
 rm -rf ./session
