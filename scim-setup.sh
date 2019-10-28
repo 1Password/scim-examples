94aac1e809df2fd5dc7ac4350d78e1561d88596b
Connor Hicks <connor@agilebits.com>
2019-10-28 17:49:35 -0400

    Delete scim-setup.sh

--- scim-setup.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-mkdir session
-
-docker run -it -v $PWD/session:'/op-scim/session' -v $HOME/.op:'/root/.op' 1password/scim:v0.8.4 /op-scim/create-session-docker.sh
-
-cp ./session/scimsession ./scimsession
-rm -rf ./session
