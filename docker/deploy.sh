0bbf186f4d264c1abb1902a21e52c4a14df59c8f
alicethorne-ab <alice.thorne@agilebits.com>
2020-05-08 19:12:10 -0400

    continuation of rewriting Docker deploy.sh file, moving code (back) into functions

--- docker/deploy.sh
+++ docker/deploy.sh
@@ -4,49 +4,9 @@
 # Please ensure you've read PREPARING.md and docker/README.md
 
 # set the full path of the docker examples directory
-docker_path=$(dirname $(realpath $0))
 
-echo "Initiating 1Password SCIM Bridge Deployment to Docker Swarm"
-echo " "
-echo "Please specify the following options."
-
-while ! [[ "$docker_type" =~ ^(swarm|compose)$ ]]; do
-    read -p "Docker Swarm or Docker Compose? [swarm/compose]: " docker_type
-done
-
-read -p "Domain name you are deploying to [e.g: 'op-scim.example.com']: " domain_name
-
-while ! [[ -f "$scimsession_file" ]]; do
-    read -p "Path to your scimsession file: " scimsession_file
-    if ! [[ -f "$scimsession_file" ]]
-    then
-        echo "File '$scimsession_file' does not exist at that path, please try again." >&2
-    fi
-done
-
-echo " "
-echo "Using the following parameters to deploy the SCIM Bridge"
-echo "Deployment type:" $docker_type
-echo "scimsession file path:" $scimsession_file
-echo "Domain name:" $domain_name
-
-while ! [[ "$proceed" =~ ^([yY][eE][sS]|[yY])$ || "$proceed" =~ ^([nN][oO][nN]) ]]; do
-    read -p "Does this look correct? [Y/n]: " proceed
-    if [[ "$proceed" =~ ^([nN][oO][nN])$ ]]
-    then
-        echo "Exiting..."
-        exit 1
-    fi
-done
-
-# place the domain name into the deployment file, in a backup
-docker_file_path=$docker_path/$docker_type
-docker_original_file=$docker_file_path/docker-compose.yml
-docker_deploy_file=$docker_file_path/docker-compose.yml.deploy
-sed s/{YOUR-DOMAIN-HERE}/$domain_name/g $docker_original_file > $docker_deploy_file
-
-if [[ "$docker_type" == "compose" ]]
-then
+# function used to set up through Docker Compose
+run_docker_compose() {
     echo " "
     echo "Deploying using Docker Compose..."
     echo "(Ctrl+C to cancel)"
@@ -64,23 +24,22 @@ then
         exit 1
     fi
 
-    while ! [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ || "$view_logs" =~ ^([nN][oO][nN]) ]]; do
-        read -p "Do you want to view the logs? [Y/n]: " view_logs
-        if [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ ]]
-        then
-            echo " "
-            echo "Press Ctrl+C to quit out of the log view."
-            sleep 2
-            docker-compose -f $docker_deploy_file logs -f 2>/dev/null
-        elif [[ "$view_logs" =~ ^([nN][oO][nN])$ ]]
-        then
-            echo "Skipping logs..."
-            echo "You can view the logs manually by running: docker-compose logs -f"
-        fi
-    done
+    read -p "Do you want to view the logs? [Y/n]: " view_logs
 
-elif [[ "$docker_type" == "swarm" ]]
-then
+    if [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo " "
+        echo "Press Ctrl+C to quit out of the log view."
+        sleep 2
+        docker-compose -f $docker_deploy_file logs -f 2>/dev/null
+    else
+        echo "Skipping logs..."
+        echo "You can view the logs manually by running: docker-compose logs -f"
+    fi
+}
+
+# function used to set up through Docker Swarm
+run_docker_swarm() {
     echo " "
     echo "Deploying using Docker Swarm..."
     echo "(Ctrl+C to cancel)"
@@ -103,20 +62,84 @@ then
         exit 1
     fi
 
-    while ! [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ || "$view_logs" =~ ^([nN][oO][nN]) ]]; do
-        read -p "Do you want to view the logs? [Y/n]: " view_logs
-        if [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ ]]
-        then
-            echo " "
-            echo "Press Ctrl+C to quit out of the log view."
-            sleep 2
-            docker service logs --raw -f op-scim_scim 2>/dev/null
-        elif [[ "$view_logs" =~ ^([nN][oO][nN])$ ]]
-        then
-            echo "Skipping logs..."
-            echo "You can view the logs manually by running: docker service logs --raw -f op-scim_scim"
-        fi
-    done
+    read -p "Do you want to view the logs? [Y/n]: " view_logs
+    if [[ "$view_logs" =~ ^([yY][eE][sS]|[yY])$ ]]
+    then
+        echo " "
+        echo "Press Ctrl+C to quit out of the log view."
+        sleep 2
+        docker service logs --raw -f op-scim_scim 2>/dev/null
+    else
+        echo "Skipping logs..."
+        echo "You can view the logs manually by running: docker service logs --raw -f op-scim_scim"
+    fi
+}
+
+# Begin main script
+
+docker_path=$(dirname $(realpath $0))
+
+echo "Initiating 1Password SCIM Bridge Deployment to Docker Swarm"
+echo " "
+echo "Please specify the following options."
+
+while :
+do
+    read -p "Docker Swarm or Docker Compose? [swarm/compose]: " docker_type
+    if [[ "$docker_type" =~ ^(swarm|compose)$ ]]; then
+        break
+    fi
+    echo "$docker_type is not a valid input. Please select either 'swarm' or 'compose'."
+done
+
+
+while :
+do
+    read -p "Fully-qualified domain name (FQDN) you are deploying to [e.g: 'op-scim.example.com']: " domain_name
+    if [[ $domain_name = *.* ]]; then
+        break
+    fi
+    echo "Please enter a fully-qualified domain name."
+done
+
+while :
+do
+    read -p "Path to your scimsession file: " scimsession_file
+    if [[ -f "$scimsession_file" ]]
+    then
+        break
+    fi
+    echo "File '$scimsession_file' does not exist at that path, please try again." >&2
+done
+
+echo " "
+echo "Using the following parameters to deploy the SCIM Bridge"
+echo "Deployment type:" $docker_type
+echo "scimsession file path:" $scimsession_file
+echo "Domain name:" $domain_name
+
+while ! [[ "$proceed" =~ ^([yY][eE][sS]|[yY])$ || "$proceed" =~ ^([nN][oO][nN]) ]]; do
+    read -p "Does this look correct? [Y/n]: " proceed
+    if [[ "$proceed" =~ ^([nN][oO][nN])$ ]]
+    then
+        echo "Exiting..."
+        exit 0 
+    fi
+done
+
+# place the domain name into the deployment file, in a backup
+docker_file_path=$docker_path/$docker_type
+docker_original_file=$docker_file_path/docker-compose.yml
+docker_deploy_file=$docker_file_path/docker-compose.yml.deploy
+sed s/{YOUR-DOMAIN-HERE}/$domain_name/g $docker_original_file > $docker_deploy_file
+
+# run the function associated with the Docker type selected
+if [[ "$docker_type" == "compose" ]]
+then
+    run_docker_compose
+elif [[ "$docker_type" == "swarm" ]]
+then
+    run_docker_swarm
 fi
 
 echo " "
