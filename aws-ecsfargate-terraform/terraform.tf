82cf54242701cf9d729f0149a060b8944b4ab0d3
Oren Mazor <oren.mazor@agilebits.com>
2021-02-12 10:27:43 -0500

    switch to using required_providers

--- aws-ecsfargate-terraform/terraform.tf
+++ aws-ecsfargate-terraform/terraform.tf
@@ -1,6 +1,13 @@
+terraform {
+  required_version = ">= 0.14.0"
+
+  required_providers {
+    aws = "~> 3.0"
+  }
+}
+
 provider "aws" {
-  version = "~> 2.0"
-  region  = var.aws_region
+  region = var.aws_region
 }
 
 data "aws_vpc" "default" {
