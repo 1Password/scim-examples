cad89d32aba224032f87ff3c7b39dd8153c27a14
Adam Pike <adam.pike@1password.com>
2021-12-23 19:59:41 -0330

    Added variable descriptions

--- aws-ecsfargate-terraform/variables.tf
+++ aws-ecsfargate-terraform/variables.tf
@@ -1,36 +1,36 @@
 variable "aws_region" {
   type        = string
-  description = ""
+  description = "The region of the AWS account where the SCIM bridge will be deployed."
 }
 
 variable "domain_name" {
   type        = string
-  description = ""
+  description = "The public DNS name pointing to the 1Password SCIM bridge."
 }
 
 variable "tags" {
   type        = map(string)
-  description = ""
+  description = "A set of tags to apply to all respective AWS resources."
 }
 
 variable "name_prefix" {
   type        = string
-  description = ""
+  description = "A default prefix to apply to the names of all AWS resources."
 }
 
 variable "vpc_name" {
   type        = string
-  description = ""
+  description = "The name of an existing VPC to use."
 }
 
 variable "wildcard_cert" {
   type        = bool
   default     = false
-  description = ""
+  description = "Set to true to use an existing wildcard certificate in ACM for the load balancer."
 }
 
 variable "using_route53" {
   type        = bool
   default     = true
-  description = ""
+  description = "Set to false to use an external DNS provider."
 }
