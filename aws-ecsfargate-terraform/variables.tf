52118461673c771a5f2aa5b4350055508707fe63
Adam Pike <adam.pike@1password.com>
2022-09-10 16:43:10 -0230

    Clarify variable description
    Signed-off-by: Adam Pike <adam.pike@1password.com>

--- aws-ecsfargate-terraform/variables.tf
+++ aws-ecsfargate-terraform/variables.tf
@@ -37,6 +37,7 @@ variable "using_route53" {
 
 variable "log_retention_days" {
   type        = number
+  description = "Specifies the number of days to retain log events in CloudWatch. The log is retained indefinitely whne set to 0."
 }
 
 # (For customers using Google Workspace participants)
