880b036871ee5e6f8b4368d59d046c9f9ed24e4b
Adam Pike <adam.pike@1password.com>
2023-08-30 11:41:52 -0230

    Update module structure
    https://developer.hashicorp.com/terraform/language/modules/develop/structure
    Signed-off-by: Adam Pike <adam.pike@1password.com>

--- aws-ecsfargate-terraform/main.tf
+++ aws-ecsfargate-terraform/main.tf
@@ -325,7 +325,7 @@ resource "aws_route53_record" "op_scim_bridge" {
 module "google_workspace" {
   count = var.using_google_workspace ? 1 : 0
 
-  source = "./aws-terraform-gw"
+  source = "./modules/google-workspace"
 
   name_prefix = local.name_prefix
   tags        = local.tags
